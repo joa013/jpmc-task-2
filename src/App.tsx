ca5da7a374113bfa96f2da51a57a714c55f2bb5d
joa013 <wachowicz.joanna@gmail.com>
2023-03-07 11:12:30 +0000

    fixed getDataFromServer()

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     let x = 0;
-    const interval = setInterval(() =>
+    const interval = setInterval(() => {
       DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
